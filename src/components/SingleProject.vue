7d2a6d91ec6b9c064cf9f8cc3f324253e8a91bc3
rider <myintthwaykhine127@gmail.com>
2022-03-23 14:46:31 +0630

    adding css and icon in SingleProject

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="project" @click="showDetail = !showDetail">
-    <h2>{{ project.title }}</h2>
+  <div class="project">
+    <div class="flexing">
+      <div>
+        <h2 @click="showDetail = !showDetail">{{ project.title }}</h2>
+      </div>
+      <div>
+        <i class="fas fa-solid fa-trash pjEditBtn"></i>
+        <i class="fas fa-solid fa-edit pjEditBtn"></i>
+        <i class="fas fa-solid fa-check pjEditBtn"></i>
+      </div>
+    </div>
     <p v-if="showDetail">{{ project.detail }}</p>
   </div>
 </template>
@@ -21,9 +30,29 @@ export default {
   background-color: #f2f2f2;
   padding: 10px 15px;
   margin: 10px;
-  cursor: pointer;
+  border-left: 6px solid red;
+  border-radius: 10px;
 }
 h2 {
   color: indigo;
+  cursor: pointer;
+  user-select: none;
+}
+h2:hover {
+  color: blue;
+}
+.flexing {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.pjEditBtn {
+  font-size: 20px;
+  margin-left: 15px;
+  user-select: none;
+}
+.pjEditBtn:hover {
+  color: #777;
+  cursor: pointer;
 }
 </style>
