cb59f77616f860a0d236cbc3e999d6c9064803d5
rider <myintthwaykhine127@gmail.com>
2022-03-23 14:08:05 +0630

    create api and fetch data in HomeView and create click to show project detail text

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="project" @click="showDetail = !showDetail">
+    <h2>{{ project.title }}</h2>
+    <p v-if="showDetail">{{ project.detail }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showDetail: false,
+    };
+  },
+  props: ["project"],
+};
+</script>
+
+<style>
+.project {
+  background-color: #f2f2f2;
+  padding: 10px 15px;
+  margin: 10px;
+  cursor: pointer;
+}
+h2 {
+  color: indigo;
+}
+</style>
